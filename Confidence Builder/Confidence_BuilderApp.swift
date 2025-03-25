90019cf3aa40d8f82418bab88df3613e41b9ea39
Mark Bush <busht24@gmail.com>
2025-03-25 09:39:18 -0400

    Initial Commit

---
+++ Confidence Builder/Confidence_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Confidence_BuilderApp.swift
+//  Confidence Builder
+//
+//  Created by MARK BUSH on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Confidence_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Confidence Builder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Confidence Builder
+//
+//  Created by MARK BUSH on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
